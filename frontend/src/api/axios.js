dafb0b16fcf279e2ed150018818d8e79ea3ec056
Davelar1789 <dnalaryea003@st.ug.edu.gh>
2025-03-07 12:12:14 +0000

    gfj

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://codewhiz-study.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "",
   withCredentials: true, // Required for sending cookies with requests
 });
 
